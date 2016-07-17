07f5415f0dd0de2f9b421d44bd12f4ca5ceaa4fc
danbahrami <dan.bahrami@googlemail.com>
2016-07-17 16:02:04 +0100

    Added jsdoc blocks to all methods in cardUtils.js

--- src/utils/cardUtils.js
+++ src/utils/cardUtils.js
@@ -4,6 +4,10 @@ import templates from "./templates"
 /**
  * cardsy.getType
  *
+ * Accepts a card number as a string or an integer and returns the
+ * card type as a capitalised string (e.g. "VISA"). If the card
+ * number matches no card type patterns it returns null.
+ *
  * @param {string|number} number - A card number
  *
  * @returns {string|null} The matching card type as found in template.js
@@ -14,6 +18,17 @@ const getType = (number) => {
     return template ? template.type : null
 }
 
+/**
+ * getTemplateByNumber (Private)
+ *
+ * Accepts a card number as a string or an integer and returns the template
+ * of the matching card type as found in src/utils/template.js. If the
+ * card number matches no card type patterns it returns null.
+ *
+ * @param {string|number} number - A card number
+ *
+ * @returns {object|null} The card template as found in src/utils/template.js
+ */
 const getTemplateByNumber = (number) => {
     if (!number) {
         return null
@@ -24,12 +39,23 @@ const getTemplateByNumber = (number) => {
     return templates.find((template) => template.pattern.test(number))
 }
 
+/**
+ * getTemplateByType (Private)
+ *
+ * Accepts a card number as a string or an integer and returns the template
+ * of the matching card type as found in src/utils/template.js. If the
+ * card number matches no card type patterns it returns null.
+ *
+ * @param {string} type - A capitalised card type (e.g. "VISA")
+ *
+ * @returns {object|null} as found in src/utils/template.js
+ */
 const getTemplateByType = (type) => {
     return templates.find((template) => template.type === type)
 }
 
 /**
- * getAutoCompleteMonth
+ * getAutoCompleteMonth (Private)
  *
  * Accepts an expiry month as a string or integer and tries
  * to return a two character string description of that 
@@ -52,6 +78,18 @@ const getAutoCompleteMonth = (month) => {
     return month
 }
 
+/**
+ * luhnCheck (Private)
+ *
+ * Accepts a card number as a string or integer and returns a boolean
+ * value for isValid based on whether the number passes a Luhn check.
+ *
+ * https://en.wikipedia.org/wiki/Luhn_algorithm
+ *
+ * @param {string|number} number - A card number
+ *
+ * @returns {boolean} Does card number pass a Luhn check
+ */
 const luhnCheck = (number) => {
     let digit
     let odd = true
