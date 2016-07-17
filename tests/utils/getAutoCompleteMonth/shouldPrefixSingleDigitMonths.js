47ffa5f3c84b1ab85ab7af508f3a822a2478ef67
danbahrami <dan.bahrami@googlemail.com>
2016-07-17 12:33:08 +0100

    Added check to month prefixing test that months starting in "1" do not get prefixed with a zero

--- tests/utils/getAutoCompleteMonth/shouldPrefixSingleDigitMonths.js
+++ tests/utils/getAutoCompleteMonth/shouldPrefixSingleDigitMonths.js
@@ -3,14 +3,17 @@ import card from "../../../src/utils/cardUtils"
 
 test("getAutoCompleteMonth should prefix single digit months", assert => {
     //Arrange
-    let formattedMonth
+    let formattedMonth, formattedIncompleteMonth
     const month = "3"
+    const incompleteMonth = "1"
 
     //Act
     formattedMonth = card.getAutoCompleteMonth(month)
+    formattedIncompleteMonth = card.getAutoCompleteMonth(incompleteMonth)
 
     //Assert
     assert.equal(formattedMonth, "03", "Prefixes single digit month with a zero")
+    assert.equal(formattedIncompleteMonth, "1", "Doesn't prefix month starting in 1")
 
     assert.end()
 })
